1afbda0d17f6bef1a7b156e330a58d89a4f87d66
Rushikesh <sorushikesh07@gmail.com>
2025-04-06 00:56:36 +0530

    Entity details

---
+++ src/main/java/com/bilsora/userManager/constants/DataBaseConstant.java
@@ -0,0 +1,19 @@
+package com.bilsora.userManager.constants;
+
+public class DataBaseConstant {
+
+  // TABLES
+  public static final String TABLE_USER = "user";
+  public static final String TABLE_ROLES = "roles";
+
+  // COLUMNS
+  public static final String COLUMN_USER_ID = "user_id";
+  public static final String COLUMN_FIRST_NAME = "first_name";
+  public static final String COLUMN_LAST_NAME = "last_name";
+  public static final String COLUMN_EMAIL = "email";
+  public static final String COLUMN_ROLE = "role";
+
+  private DataBaseConstant() {
+    // private constructor
+  }
+}
--- src/main/java/com/bilsora/userManager/entity/User.java
+++ src/main/java/com/bilsora/userManager/entity/User.java
@@ -1,4 +1,30 @@
 package com.bilsora.userManager.entity;
 
+import com.bilsora.userManager.constants.DataBaseConstant;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = DataBaseConstant.TABLE_USER)
 public class User {
+
+    @Id
+    @Column(name = DataBaseConstant.COLUMN_USER_ID)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = DataBaseConstant.COLUMN_FIRST_NAME)
+    private String firstName;
+
+    @Column(name = DataBaseConstant.COLUMN_LAST_NAME)
+    private String lastName;
+
+    @Column(name = DataBaseConstant.COLUMN_EMAIL, unique = true)
+    private String email;
+
+    @Column(name = DataBaseConstant.COLUMN_ROLE)
+    private String role;
 }
