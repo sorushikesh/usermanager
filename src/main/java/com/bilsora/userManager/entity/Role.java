889ffaf0496331a609942b40d5b81ddb8bc8d9f7
Rushikesh <sorushikesh07@gmail.com>
2025-04-06 00:56:36 +0530

    Entity details

---
+++ src/main/java/com/bilsora/userManager/entity/Role.java
@@ -0,0 +1,20 @@
+package com.bilsora.userManager.entity;
+
+import com.bilsora.userManager.constants.DataBaseConstant;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = DataBaseConstant.TABLE_ROLES)
+public class Role {
+
+  @Id
+  @Column(name = DataBaseConstant.COLUMN_USER_ID)
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+
+}
