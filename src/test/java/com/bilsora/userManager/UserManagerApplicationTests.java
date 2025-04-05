da3af53bc943336b7699c9c109c987df956e96fa
Rushikesh <sorushikesh07@gmail.com>
2025-04-06 00:56:24 +0530

    Code format

--- src/test/java/com/bilsora/userManager/UserManagerApplicationTests.java
+++ src/test/java/com/bilsora/userManager/UserManagerApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
