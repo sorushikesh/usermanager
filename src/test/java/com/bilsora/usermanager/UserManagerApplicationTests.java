51016c69598dd6a838df0cba944b6fb6130ce7d4
Rushikesh <sorushikesh07@gmail.com>
2025-04-27 23:30:57 +0530

    User role based auth

--- src/test/java/com/bilsora/usermanager/UserManagerApplicationTests.java
+++ src/test/java/com/bilsora/usermanager/UserManagerApplicationTests.java
@@ -1,13 +1,6 @@
 package com.bilsora.usermanager;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class UserManagerApplicationTests {}
