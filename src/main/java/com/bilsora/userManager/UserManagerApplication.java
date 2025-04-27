3d49f30e0325269b4f9eb1c62711130ccd8725c8
Rushikesh <sorushikesh07@gmail.com>
2025-04-27 15:09:41 +0530

    Code format

--- src/main/java/com/bilsora/userManager/UserManagerApplication.java
+++ src/main/java/com/bilsora/userManager/UserManagerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserManagerApplication.class, args);
+  }
 }
