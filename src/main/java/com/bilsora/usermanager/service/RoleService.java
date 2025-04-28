bb358dd940adc6f6f0745baf5e303bdf5b23aaa8
Rushikesh <sorushikesh07@gmail.com>
2025-04-29 02:33:54 +0530

    Doc and GetAllRoles method

--- src/main/java/com/bilsora/usermanager/service/RoleService.java
+++ src/main/java/com/bilsora/usermanager/service/RoleService.java
@@ -1,9 +1,26 @@
 package com.bilsora.usermanager.service;
 
 import com.bilsora.usermanager.dto.response.RoleResponse;
-import com.bilsora.usermanager.exceptions.NotFoundException;
 
+import java.util.List;
+
+/**
+ * Service interface for managing roles.
+ */
 public interface RoleService {
 
-  RoleResponse findByName(String roleName) throws NotFoundException;
+  /**
+   * Find a role by its name.
+   *
+   * @param roleName the name of the role
+   * @return the RoleResponse
+   */
+  RoleResponse findByName(String roleName);
+
+  /**
+   * Get all available roles.
+   *
+   * @return list of RoleResponse
+   */
+  List<RoleResponse> getAllRoles();
 }
