44fb85860441451ebb2de7aab0b914d26bcbb45a
Rushikesh <sorushikesh07@gmail.com>
2025-04-29 01:29:58 +0530

    Exception handling

---
+++ src/main/java/com/bilsora/usermanager/constants/ExceptionErrorCode.java
@@ -0,0 +1,10 @@
+package com.bilsora.usermanager.constants;
+
+public class ExceptionErrorCode {
+
+  public static final String EXCEPTION_NOT_FOUND = "BLS_2001_NOT_FOUND";
+
+  private ExceptionErrorCode() {
+    // private constructor
+  }
+}
---
+++ src/main/java/com/bilsora/usermanager/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.bilsora.usermanager.exceptions;
+
+import com.bilsora.usermanager.constants.ExceptionErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private final MessageSource messageSource;
+
+  public GlobalExceptionHandler(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ProblemDetail handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
+
+    ProblemDetail problemDetail = ex.getBody();
+    problemDetail.setStatus(HttpStatus.NOT_FOUND);
+
+    var errorCode = ExceptionErrorCode.EXCEPTION_NOT_FOUND;
+    var errorMessage =
+        Optional.of(
+            messageSource.getMessage(
+                errorCode, ex.getDetailMessageArguments(), LocaleContextHolder.getLocale()));
+
+    problemDetail.setProperty("path", request.getRequestURI());
+    problemDetail.setProperty("ErrorCode", errorCode + ":" + errorMessage);
+
+    return problemDetail;
+  }
+}
