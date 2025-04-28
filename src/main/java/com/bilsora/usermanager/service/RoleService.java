d43b725aa4e217fff5f3bb8b002e9deaa40db6f7
Rushikesh <sorushikesh07@gmail.com>
2025-04-28 18:36:06 +0530

    Liquibase: Create roles

--- src/main/java/com/bilsora/usermanager/service/RoleService.java
+++ src/main/java/com/bilsora/usermanager/service/RoleService.java
@@ -1,8 +1,6 @@
 package com.bilsora.usermanager.service;
 
-import com.bilsora.usermanager.dto.RoleCreationRequest;
-
 public interface RoleService {
 
-  void createRole(RoleCreationRequest request);
+
 }
