5fb551ae336d52d1fb7fa75fd78c64f70f15a6d5
Rushikesh <sorushikesh07@gmail.com>
2025-04-29 01:38:41 +0530

    Field Constant

---
+++ src/main/java/com/bilsora/usermanager/constants/FieldConstant.java
@@ -0,0 +1,10 @@
+package com.bilsora.usermanager.constants;
+
+public class FieldConstant {
+
+  public static final String ROLE = "Role";
+
+  private FieldConstant() {
+    // private constructor
+  }
+}
