ccaa75c86a28151f6278b085f52002d68c01920a
Rushikesh <sorushikesh07@gmail.com>
2025-04-28 18:36:06 +0530

    Liquibase: Create roles

--- src/main/java/com/bilsora/usermanager/dto/RoleCreationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bilsora.usermanager.dto;
-
-import lombok.Data;
-
-@Data
-public class RoleCreationRequest {
-
-  private String roleName;
-}
--- src/main/java/com/bilsora/usermanager/service/serviceImpl/RoleServiceImpl.java
+++ src/main/java/com/bilsora/usermanager/service/serviceImpl/RoleServiceImpl.java
@@ -1,7 +1,5 @@
 package com.bilsora.usermanager.service.serviceImpl;
 
-import com.bilsora.usermanager.dto.RoleCreationRequest;
-import com.bilsora.usermanager.model.Role;
 import com.bilsora.usermanager.repository.RoleRepository;
 import com.bilsora.usermanager.service.RoleService;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +11,4 @@ public class RoleServiceImpl implements RoleService {
 
   private final RoleRepository roleRepository;
 
-  public void createRole(RoleCreationRequest request) {
-    if (roleRepository.findByName(request.getRoleName()).isPresent()) {
-      throw new RuntimeException("Role already exists: " + request.getRoleName());
-    }
-
-    Role role = new Role(request.getRoleName());
-
-    roleRepository.save(role);
-  }
 }
